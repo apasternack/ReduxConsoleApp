38b7bbfad1b16bf0a6506becebb752db60ef3368
Adam Pasternack <adam.pasternack@huntington.com>
2017-05-17 11:37:21 -0400

    dispatching a change of state to the store

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function reducer(state, action) {
   return state;
 }
 
-const store = createStore(reducer, defaultState); 
+const store = createStore(reducer, defaultState);
 
 function addView(viewFunc) {
   viewFunc(defaultState);
@@ -35,13 +35,14 @@ addView((state) => {
 });
 
 addView((state) => {
-  console.log(`The latest course in the library: ${state.courses[state.courses.length -1].name}`);
+  console.log(`The latest course in the library: ${state.courses[state.courses.length - 1].name}`);
 });
 
-defaultState.courses.push({
-  name: 'This is the new course',
-  topic: 'Really does not matter'
-});
-
-
+store.dispatch({
+  type: 'ADD_COURSE',
+  course: {
+    name: 'This is the new course',
+    topic: 'Really does not matter'
+  }
+})
 
