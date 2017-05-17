11f674f41f41b650a1892efa0ccb1f3acce7e69a
Adam Pasternack <adam.pasternack@huntington.com>
2017-05-17 11:08:08 -0400

    Building a store and reducer

---
+++ app.js
@@ -0,0 +1,45 @@
+const { createSotre } = require('redux');
+
+const defaultState = {
+  courses: [
+    {
+      name: 'Learning React',
+      topic: 'React',
+    },
+    {
+      name: 'Learning Angular',
+      topic: 'Angular',
+    },
+    {
+      name: 'Using Redux with Angular',
+      topic: 'Angular',
+    }
+  ]
+};
+
+function reducer(state, action) {
+  return state;
+}
+
+const store = createStore(reducer, defaultState); 
+
+function addView(viewFunc) {
+  viewFunc(defaultState);
+}
+
+addView((state) => {
+  console.log(`There are ${state.courses.length} courses in the library`);
+});
+
+addView((state) => {
+  console.log(`The latest course in the library: ${state.courses[state.courses.length -1].name}`);
+});
+
+defaultState.courses.push({
+  name: 'This is the new course',
+  topic: 'Really does not matter'
+});
+
+
+
+
